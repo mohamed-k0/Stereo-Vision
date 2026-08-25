ce9b3f136774b997886f5a594a250a21118ab91c
mohamed-k0 <mohamed.khaled.sh22@gmail.com>
2026-08-25 10:14:24 +0300

    reading left and right images

--- stereo-vision.py
+++ stereo-vision.py
@@ -1,1 +1,17 @@
-import cv2 
+import cv2 as cv
+
+# Load left and right images in Grayscale
+left_image = cv.imread("dataset/im0/im0.png", cv.IMREAD_GRAYSCALE)
+right_image = cv.imread("dataset/im0/im1.png", cv.IMREAD_GRAYSCALE)
+
+print("Left image shape:", left_image.shape)
+print("Right image shape:", right_image.shape)
+
+# Create Block Matching "object"
+block_matching = cv.StereoBM_create(numDisparities=272, blockSize=15)
+
+# Calculate disparity using compute method
+disparity = block_matching.compute(left_image, right_image)
+
+print(disparity.shape)
+
