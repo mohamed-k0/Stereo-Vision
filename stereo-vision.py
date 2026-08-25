fcff2ca3ea3e091178071b87f97c191c83ffe00d
mohamed-k0 <mohamed.khaled.sh22@gmail.com>
2026-08-25 10:38:09 +0300

    normalizing the disparity raw value and displaying the disparity map

--- stereo-vision.py
+++ stereo-vision.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import numpy as np
 
 # Load left and right images in Grayscale
 left_image = cv.imread("dataset/im0/im0.png", cv.IMREAD_GRAYSCALE)
@@ -18,7 +19,11 @@ print(disparity.shape)
 # Normalize disparity for visualization
 visual_disparity = cv.normalize(disparity, None, 0, 255, cv.NORM_MINMAX)
 
-cv.imshow(visual_disparity)
+# Convert to 8-bit unsigned integer
+visual_disparity = visual_disparity.astype(np.uint8)
+
+# Showing the Disparity Map
+cv.imshow("window",visual_disparity)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
